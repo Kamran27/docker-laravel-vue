e432a4d2c9478f75e65e181e20f160031630f3ae
Kamran Gaibkhanov <kamrangaibkhanov@gmail.com>
2022-03-11 11:30:35 +0100

    Add new protected/unprotected routes for theses and tags git add routes/api.php

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ThesisController;
+use App\Http\Controllers\TagsController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,24 +17,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/theses', [ThesisController::class, 'index']);
+Route::get('/theses/{id}', [ThesisController::class, 'show']);
+Route::get('/theses/search/{titel}', [ThesisController::class, 'search']);
+
+Route::get('/tags', [TagsController::class, 'index']);
+Route::get('/tags/{id}', [TagsController::class, 'show']);
+
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::post('/theses', [ThesisController::class, 'store']);
+    Route::put('/theses/{id}', [ThesisController::class, 'update']);
+    Route::delete('/theses/{id}', [ThesisController::class, 'destroy']); 
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/tags', [TagsController::class, 'store']);
+    Route::put('/tags/{id}', [TagsController::class, 'update']);
+    Route::delete('/tags/{id}', [TagsController::class, 'destroy']); 
+});
+
+
+
+
+
+//Protected routes
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
 
  //Get Theses
- Route::get('getTheses',[App\Http\Controllers\ThesisController::class, 'getTheses']);
-
+ //Route::get('getTheses',[App\Http\Controllers\ThesisController::class, 'getTheses']);
+ 
  //Get Tags
- Route::get('getTags',[App\Http\Controllers\ThesisController::class, 'getTags']);
+ //Route::get('getTags',[App\Http\Controllers\ThesisController::class, 'getTags']);
 
  //Get Thesis detail
- Route::get('get_thesis/{id}',[App\Http\Controllers\ThesisController::class, 'get_thesis']);
+ //Route::get('get_thesis/{id}',[App\Http\Controllers\ThesisController::class, 'get_thesis']);
 
  //Save Theses
- Route::post('saveTheses',[App\Http\Controllers\ThesisController::class, 'saveTheses']);
+ //Route::post('saveTheses',[App\Http\Controllers\ThesisController::class, 'saveTheses']);
+ 
 
  //Update Theses
- Route::post('updateTheses/{id}',[App\Http\Controllers\ThesisController::class, 'updateTheses']);
+ //Route::post('updateTheses/{id}',[App\Http\Controllers\ThesisController::class, 'updateTheses']);
 
  //Save Theses
- Route::delete('deleteThesis/{id}',[App\Http\Controllers\ThesisController::class, 'deleteThesis']);
+ //Route::delete('deleteThesis/{id}',[App\Http\Controllers\ThesisController::class, 'deleteThesis']);
