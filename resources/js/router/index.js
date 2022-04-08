aea5e9936ee7d79279c75e0481311d1026c0bd42
Kamran Gaibkhanov <kamrangaibkhanov@gmail.com>
2022-04-08 16:52:24 +0200

    [Frontend] Add routes and actions for login and registration

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -3,16 +3,30 @@ import VueRouter from 'vue-router';
 Vue.use(VueRouter);
 
 import ThesesList from '../components/ThesesList.vue';
-import ThesesListAdmin from '../components/ThesesListAdmin.vue';
+import AdminThesesList from '../components/AdminThesesList.vue';
 import Home from '../components/Home.vue';
 import ThesisDetail from '../components/ThesisDetail.vue';
 import TagsList from '../components/TagsList.vue';
+import AdminUserList from '../components/AdminUserList.vue';
+import UserLogin from '../components/UserLogin.vue';
+import UserRegistration from '../components/UserRegistration.vue';
+import Admin from '../components/Admin.vue';
 
 const routes = [
     {
         name: '/',
         path: '/',
         component: Home
+    },
+    {
+        name: '/login',
+        path: '/login',
+        component: UserLogin
+    },
+    {
+        name: '/registration',
+        path: '/registration',
+        component: UserRegistration
     },
     {
         name: '/theses',
@@ -22,7 +36,27 @@ const routes = [
     {
         name: '/admin',
         path: '/admin',
-        component: ThesesListAdmin
+        component: Admin,
+        beforeEnter: (to, from, next) => {
+            let currentUser = JSON.parse(window.localStorage.currentUser);
+            if(currentUser && currentUser.admin) {
+                next();
+            } else {
+                next("/");
+            }
+          },
+          children: [
+            {
+                name: 'theses',
+                path: 'theses',
+                component: AdminThesesList
+            },
+            {
+                name: 'users',
+                path: 'users',
+                component: AdminUserList
+            },
+          ],
     },
     {
         name: '/get_thesis',
@@ -34,6 +68,7 @@ const routes = [
         path: '/get_tag/:id?',
         component: TagsList
     },
+   
 ];
 
  const router = new VueRouter({
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -8,6 +8,8 @@ export default new Vuex.Store({
     state: {
         theses: [],
         tags: [],
+        users: [],
+        currentUser: {},
     },
     mutations: {
         SET_THESES(state, theses) {
@@ -16,6 +18,9 @@ export default new Vuex.Store({
         SET_TAGS(state, tags) {
             state.tags = tags;
         },
+        SET_USERS(state, users) {
+            state.users = users;
+        },
         ADD_THESES(state, thesis) {
             let theses =  state.theses.concat(thesis);
             state.theses = theses;
@@ -23,14 +28,21 @@ export default new Vuex.Store({
         EDIT_THESES(state, thesis) {
             state.theses.forEach(t=> {
                 if (t.id == thesis.id) {
-                    t = thesis;
-                    
+                    t = thesis;             
                 }
             })
         },
         DELETE_THESES(state, thesisId) {
             let theses =  state.theses.filter(t=>t.id != thesisId);
             state.theses = theses;
+        },
+        LOGOUT_USER(state) {
+            state.currentUser = {}
+            window.localStorage.currentUser = JSON.stringify({});
+        },
+        SET_CURRENT_USER(state, user) {
+            state.currentUser = user;
+            window.localStorage.currentUser = JSON.stringify(user);
         },
     },
     actions: {
@@ -59,6 +71,23 @@ export default new Vuex.Store({
             //commit('SET_TAGS', tags);
             commit('SET_TAGS', tags.map(t => t.attributes));    
         },
+        async loadUsers({commit}) {  
+            let responseUsers = await Api().get('/users');
+            //debugger
+            let users = responseUsers.data.data;
+            users.forEach(u=> {
+                u.attributes.id = u.id;
+            });
+            //debugger
+            commit('SET_USERS', users.map(u => u.attributes));
+            
+            /* let user = JSON.parse(window.localStorage.currentUser);
+            commit('SET_CURRENT_USER', user); */
+        },
+        async loadCurrentUser({commit}) {  
+            let user = JSON.parse(window.localStorage.currentUser);
+            commit('SET_CURRENT_USER', user);
+        },
         async createThesis({commit}, thesis) { 
             let response = await Api().post('/theses', thesis);
             let savedThesis = response.data.data.attributes;
@@ -67,7 +96,7 @@ export default new Vuex.Store({
         },
         async editThesis({commit}, thesis) { 
             let response = await Api().put(`/theses/${thesis.id}`, thesis);
-            debugger
+            //debugger
             let editedThesis = response.data.data.attributes;
             
             commit('EDIT_THESES', editedThesis);
@@ -79,6 +108,43 @@ export default new Vuex.Store({
                 commit('DELETE_THESES', thesis.id);
             } 
         },
+        logoutUser({commit}) {
+            commit('LOGOUT_USER')
+        },
+        async loginUser({commit}, loginInfo) {
+             try {
+                let response = await Api().post('/login', loginInfo);
+                //debugger
+                let user = response.data.user;
+                
+                commit('SET_CURRENT_USER', user);
+                return user;
+             } catch {
+                 return {error: 
+                    "Email/password Kombination war leider falsch. Bitte, versuchen Sie nochmal."}
+             }
+             
+        },
+        async registerUser({commit}, registerInfo) {
+            try {
+               let response = await Api().post('/register', registerInfo);
+               let user = response.data.user;
+               debugger
+               //let ustok = response.data;
+               //let token = response.data.token;
+               
+              /*  ustok.forEach(u=> {
+                     u.user.token = u.token;
+               }); */   
+               //debugger
+               commit('SET_CURRENT_USER', user);
+               //commit('SET_USERS', user);
+               return user;
+            } catch {
+                return {error: "Es gab einen Fehler. Bitte versuchen Sie nochmal"}
+            }
+            
+       },
     },
     getters: {
         getTag: state => id => {   
